79b745286a5248ffe4a3a7811d2f4c594dc1b6f0
Jacob Proffer <jacobhardingproffer@gmail.com>
2016-09-28 00:05:18 -0400

    Add keywords and description meta tags

--- header.php
+++ header.php
@@ -2,8 +2,10 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1"/>
     <title>Document</title>
+    <meta name="keywords" content="keyword" />
+    <meta name="description" content="description" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1"/>
     <link rel="stylesheet" href="assets/css/project-main.css">
   </head>
   <body>
