f8b85b3843f5e1e23bfc8ccd379b139dd8e9736d
jacobproffer <jacob@elegantseagulls.com>
2016-11-03 11:57:23 -0400

    Update browser selection for browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('serve', ['sass', 'js'], function() {
 
     browserSync.init({
         server: './',
-        browser: "google chrome canary"
+        browser: "google chrome"
     });
 
     gulp.watch('assets/scss/**/*.scss', ['sass']);
