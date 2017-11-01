3889d0feea80e55e122b0424efa5a1395a956317
jacobproffer <jacobhardingproffer@gmail.com>
2017-11-01 10:35:21 -0400

    Add html minification and updated shell files

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -7,26 +7,33 @@ var gulp        = require('gulp'),
     prefix      = require('gulp-autoprefixer'),
     uglify      = require('gulp-uglify'),
     concat      = require('gulp-concat'),
+    htmlmin     = require('gulp-htmlmin'),
     browserSync = require('browser-sync').create();
 
 var scripts = [
-  '../assets/js/lib/jquery-3.2.1.min.js',
   '../assets/js/lib/headroom/headroom.min.js',
   '../assets/js/lib/headroom/jQuery.headroom.js',
   '../assets/js/app.js'
 ];
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js'], function() {
+gulp.task('serve', ['html', 'sass', 'js'], function() {
 
     browserSync.init({
-        server: '../',
+        server: '../dist/',
         browser: "google chrome"
     });
 
+    gulp.watch('../*.html', ['html']);
     gulp.watch('../assets/scss/**/*.scss', ['sass']);
     gulp.watch('../assets/js/**/*.js', ['js']);
     gulp.watch('../*.html').on('change', browserSync.reload);
+});
+
+gulp.task('html', function() {
+  return gulp.src('../*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('../dist'));
 });
 
 // Configure CSS tasks.
@@ -50,10 +57,4 @@ gulp.task('js', function() {
     .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-  gulp.watch('../dist/scss/**/*.scss', ['sass']);
-  gulp.watch('../dist/js/**/*.js', ['js']);
-  gulp.watch('../*.html').on('change', browserSync.reload);
-});
-
-gulp.task('default', ['sass', 'js', 'serve']);
+gulp.task('default', ['html', 'sass', 'js', 'serve']);
