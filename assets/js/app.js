a18d78549078074a3e446208c36f1cbab894ad83
jacobproffer <jacobhardingproffer@gmail.com>
2018-03-16 13:41:47 -0400

    Remove headroom and clean-up gulpfile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,18 @@
 // Variables
-var body = $('html, body'),
-		mobileMenu = $('.mobile-nav-button'),
-		mainNavigation = $('.main-nav'),
-		mainHeader = $('.main-header'),
-		links = $('a[href*="#"]'),
-		headerHeight = mainHeader.outerHeight();
+var body = $('html, body');
+var mobileMenu = $('.mobile-nav-button');
+var mainNavigation = $('.main-nav');
+var mainHeader = $('.main-header');
+var links = $('a[href*="#"]');
+var headerHeight = mainHeader.outerHeight();
 
 // Mobile menu function
 mobileMenu.click(function() {
-	mainNavigation.toggleClass('nav-open');
-	$(this).toggleClass('nav-open');
-	mainHeader.toggleClass('open');
-	body.toggleClass('body-modal-open');
-	body.toggleClass('disable-scrolling');
+  mainNavigation.toggleClass('nav-open');
+  $(this).toggleClass('nav-open');
+  mainHeader.toggleClass('open');
+  body.toggleClass('body-modal-open');
+  body.toggleClass('disable-scrolling');
 });
 
 /*
@@ -20,48 +20,28 @@ mobileMenu.click(function() {
 	 the mobile menu that is on the same page
 */
 links.click(function() {
-	mainNavigation.removeClass('nav-open');
-	mainHeader.removeClass('open');
-	mobileMenu.removeClass('nav-open');
-	body.removeClass('body-modal-open');
-	body.removeClass('disable-scrolling');
-});
-
-// Headroom.js settings and functions
-mainHeader.headroom({
-  offset    : headerHeight,
-  tolerance   : { up:10, down:10 },
-  classes : {
-    pinned   : "pinned",
-    unpinned : "unpinned",
-    top      : "onTop",
-    bottom   : "onBottom",
-    notTop   : "scrolled"
-  },
-	onUnpin : function() {
-		if ( mainHeader.hasClass('open') ) {
-			mainHeader.removeClass('unpinned');
-		}
-	},
-  onTop : function() {
-    mainHeader.removeClass('pinned');
-  }
+  mainNavigation.removeClass('nav-open');
+  mainHeader.removeClass('open');
+  mobileMenu.removeClass('nav-open');
+  body.removeClass('body-modal-open');
+  body.removeClass('disable-scrolling');
 });
 
 /*
 	 Removes ability to scroll background
 	 content on iOS when a modal is open.
 */
-document.ontouchmove = function ( event ) {
-	var isTouchMoveAllowed = true, target = event.target;
-	while ( target !== null ) {
-    if ( target.classList && target.classList.contains( 'disable-scrolling' ) ) {
-	    isTouchMoveAllowed = false;
-	    break;
+document.ontouchmove = function(event) {
+  var isTouchMoveAllowed = true,
+    target = event.target;
+  while (target !== null) {
+    if (target.classList && target.classList.contains('disable-scrolling')) {
+      isTouchMoveAllowed = false;
+      break;
     }
-	  target = target.parentNode;
-	}
-	if ( !isTouchMoveAllowed ) {
-	  event.preventDefault();
-	}
+    target = target.parentNode;
+  }
+  if (!isTouchMoveAllowed) {
+    event.preventDefault();
+  }
 };
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,56 +1,62 @@
 'use strict';
 
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    sassLint    = require('gulp-sass-lint'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    rename      = require('gulp-rename'),
-    prefix      = require('gulp-autoprefixer'),
-    uglify      = require('gulp-uglify'),
-    concat      = require('gulp-concat'),
-    htmlmin     = require('gulp-htmlmin'),
-    browserSync = require('browser-sync').create();
-
-var scripts = [
-  '../assets/js/lib/headroom/headroom.min.js',
-  '../assets/js/lib/headroom/jQuery.headroom.js',
-  '../assets/js/app.js'
-];
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var prefix = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var htmlmin = require('gulp-htmlmin');
+var browserSync = require('browser-sync').create();
+var scripts = ['../assets/js/app.js'];
 
 gulp.task('html', function() {
-  return gulp.src('../*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+  return gulp
+    .src('../*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('../dist'));
 });
 
 // Linter
 gulp.task('sass-lint', function() {
-  return gulp.src('../assets/scss/**/*.scss')
-  .pipe(sassLint({
-    configFile: '.scss-lint-config.yml'
-  }))
-  .pipe(sassLint.format())
-  .pipe(sassLint.failOnError())
+  return gulp
+    .src('../assets/scss/**/*.scss')
+    .pipe(
+      sassLint({
+        configFile: '.scss-lint-config.yml'
+      })
+    )
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
 });
 
 // Configure CSS tasks.
-gulp.task('sass', gulp.series('sass-lint'), function () {
-  return gulp.src('../assets/scss/**/*.scss')
+gulp.task('sass', gulp.series('sass-lint'), function() {
+  return gulp
+    .src('../assets/scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write('.', {sourceRoot:'../../assets/scss',includeContent: false}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(
+      sourcemaps.write('.', {
+        sourceRoot: '../../assets/scss',
+        includeContent: false
+      })
+    )
     .pipe(gulp.dest('../dist/css'))
     .pipe(browserSync.stream());
 });
 
 // Configure JS.
 gulp.task('js', function() {
-  return gulp.src(scripts)
+  return gulp
+    .src(scripts)
     .pipe(uglify())
     .pipe(concat('app.js'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('../dist/js'))
     .pipe(browserSync.stream());
 });
@@ -59,16 +65,21 @@ gulp.task('js', function() {
 gulp.task('browser-sync', function(done) {
   browserSync.init({
     server: '../dist/',
-    browser: "google chrome"
+    browser: 'google chrome'
   });
   done();
 });
 
-gulp.task('default', gulp.series(gulp.parallel('html', 'sass', 'js'), 'browser-sync',
-  function watcher(done) {
-    gulp.watch('../*.html', gulp.parallel('html'));
-    gulp.watch('../assets/scss/**/*.scss', gulp.parallel('sass'));
-    gulp.watch('../assets/js/**/*.js', gulp.parallel('js'));
-    gulp.watch('../*.html').on('change', browserSync.reload);
-  }
-));
+gulp.task(
+  'default',
+  gulp.series(
+    gulp.parallel('html', 'sass', 'js'),
+    'browser-sync',
+    function watcher(done) {
+      gulp.watch('../*.html', gulp.parallel('html'));
+      gulp.watch('../assets/scss/**/*.scss', gulp.parallel('sass'));
+      gulp.watch('../assets/js/**/*.js', gulp.parallel('js'));
+      gulp.watch('../*.html').on('change', browserSync.reload);
+    }
+  )
+);
